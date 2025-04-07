724cd089dd80649cd0bcf3c9fc31423b78c84167
soo5o <jungsk20@gmail.com>
2025-04-07 17:48:57 +0900

    Feat: 달력 라벨 구성 완료

--- src/pages/CalendarPage.vue
+++ src/pages/CalendarPage.vue
@@ -14,9 +14,41 @@ const calendarOptions = {
   plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
   initialView: 'dayGridMonth',
   events: [
-    { title: '지출', start: '2025-04-08', color: '#ff6060' },
-    { title: '수익', start: '2025-04-08', end: '2025-04-11', color: '#60b7ff' },
+    {
+      title: '지출',
+      start: '2025-04-08',
+      color: '#ff6060',
+      extendedProps: {
+        borderColor: '#ff384c',
+        opacity: 0.8,
+      },
+    },
+    {
+      title: '수익',
+      start: '2025-04-08',
+      end: '2025-04-11',
+      color: '#60b7ff',
+      extendedProps: {
+        borderColor: '#4c9dff',
+        opacity: 0.8,
+      },
+    },
   ],
+  eventDidMount(info) {
+    const { borderColor, opacity } = info.event.extendedProps;
+    if (borderColor) {
+      info.el.style.border = `2px solid ${borderColor}`;
+    }
+    if (opacity) {
+      info.el.style.backgroundColor = info.el.style.backgroundColor
+        .replace('rgb', 'rgba')
+        .replace(')', `, ${opacity})`);
+    }
+  },
 };
 </script>
-<style scoped></style>
+<style scoped>
+* {
+  color: #4c9dff;
+}
+</style>
