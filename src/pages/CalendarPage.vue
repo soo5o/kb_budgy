fc363ae692bea012ac5478d70a61bc6c658d282b
soo5o <jungsk20@gmail.com>
2025-04-07 17:35:05 +0900

    Feat: 캘린더 불러오기 기능

---
+++ src/pages/CalendarPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container mt-3">
+    <FullCalendar :options="calendarOptions" />
+  </div>
+</template>
+<script setup>
+import FullCalendar from '@fullcalendar/vue3';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import koLocale from '@fullcalendar/core/locales/ko';
+const calendarOptions = {
+  //   locale: koLocale, // ← 한국어 설정
+  plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
+  initialView: 'dayGridMonth',
+  events: [
+    { title: '지출', start: '2025-04-08', color: '#ff6060' },
+    { title: '수익', start: '2025-04-08', end: '2025-04-11', color: '#60b7ff' },
+  ],
+};
+</script>
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import LoginPage from '@/pages/LoginPage.vue';
 import SignUpPage from '@/pages/SignUpPage.vue';
 import HomePage from '@/pages/HomePage.vue';
 import ChartPage from '@/pages/ChartPage.vue';
+import CalendarPage from '@/pages/CalendarPage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -31,6 +32,11 @@ const router = createRouter({
       path: '/chart/:userId',
       name: 'chart',
       component: ChartPage,
+    },
+    {
+      path: '/calendar',
+      name: 'calendar',
+      component: CalendarPage,
     },
   ],
 });
