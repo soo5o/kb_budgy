ed70cd9ac6519eeab8cf970396a9dfe41bcb74f6
yellowmodric <kyr7091@naver.com>
2025-04-09 13:29:09 +0900

    아직 하는 중

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,12 +4,37 @@
     <div class="home-container"></div>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import TotalAssetBox from '@/components/TotalAssetBox.vue';
+import { useUserStore } from '@/stores/user';
+import { useRouter } from 'vue-router';
+import axios from 'axios';
+
+const moneyList = ref([]);
+const userStore = useUserStore();
+const router = useRouter();
+
+const fetchData = async() => {
+  const userId = userStore.userInfo[0]?.id || 0;
+
+  if (!userId) {
+    alert('로그인이 필요합니다!');
+    router.push('/login');
+    return;
+  }
+
+  const {data} = await axios.get(`http://localhost:3000/money?userId=${useId}`);
+
+  moneyList.value = data.sort((a, b) => new Date(b.consumptionDate) - )
+}
+import { computed, useId } from 'vue';
+
+const netAsset = computed(() => {});
+</script>
 
 <style scoped>
 .home-container {
   width: 480px;
   padding: 2rem;
-  background-color: aquamarine;
 }
 </style>
