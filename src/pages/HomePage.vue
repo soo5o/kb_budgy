be39d9807e6226eaa9b7f392e57811f1722fad7e
yellowmodric <kyr7091@naver.com>
2025-04-09 14:43:00 +0900

    홈페이지 완료

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,21 +1,31 @@
 <!-- HomePage.vue -->
 <template>
   <div class="d-flex justify-content-center">
-    <div class="home-container"></div>
+    <div class="home-container">
+      <TotalAssetBox :total="netAsset"></TotalAssetBox>
+      <RecentHistory :items="recentItems"></RecentHistory>
+      <div class="text-end mt-3">
+        <button class="btn btn-custom w-100" @click="goToDetail">더보기</button>
+      </div>
+    </div>
   </div>
 </template>
 <script setup>
 import TotalAssetBox from '@/components/TotalAssetBox.vue';
+import RecentHistory from '@/components/RecentHistory.vue';
 import { useUserStore } from '@/stores/user';
 import { useRouter } from 'vue-router';
 import axios from 'axios';
+import { onMounted, ref } from 'vue';
+import { computed } from 'vue';
 
 const moneyList = ref([]);
 const userStore = useUserStore();
 const router = useRouter();
 
-const fetchData = async() => {
-  const userId = userStore.userInfo[0]?.id || 0;
+const fetchData = async () => {
+  const userId = computed(() => userStore.userInfo[0]?.id || 0);
+  console.log(userId.value);
 
   if (!userId) {
     alert('로그인이 필요합니다!');
@@ -23,18 +33,54 @@ const fetchData = async() => {
     return;
   }
 
-  const {data} = await axios.get(`http://localhost:3000/money?userId=${useId}`);
+  const { data } = await axios.get(
+    `http://localhost:3000/money?userId=${String(userId.value)}`
+  );
 
-  moneyList.value = data.sort((a, b) => new Date(b.consumptionDate) - )
-}
-import { computed, useId } from 'vue';
+  moneyList.value = data.sort(
+    (a, b) => new Date(b.consumptionDate) - new Date(a.consumptionDate)
+  );
+};
 
-const netAsset = computed(() => {});
+onMounted(() => {
+  fetchData();
+});
+
+const recentItems = computed(() => moneyList.value.slice(0, 5));
+
+const netAsset = computed(() => {
+  const income = moneyList.value
+    .filter((i) => i.type === 'income')
+    .reduce((acc, cur) => acc + Number(cur.amount), 0);
+
+  const expense = moneyList.value
+    .filter((i) => i.type === 'expense')
+    .reduce((acc, cur) => acc + Number(cur.amount), 0);
+
+  return income - expense;
+});
+
+const goToDetail = () => {
+  router.push('/detail');
+};
 </script>
 
 <style scoped>
 .home-container {
   width: 480px;
   padding: 2rem;
+}
+.btn-custom {
+  background-color: #f7fbf9;
+  color: #4fcca4;
+  border: 1px solid #4fcca4;
+  border-radius: 20px;
+  padding: 1rem;
+  font-weight: 600;
+  transition: all 0.2s ease-in-out;
+}
+.btn-custom:hover {
+  background-color: #4fcca4;
+  color: white;
 }
 </style>
