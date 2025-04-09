98d4935625d783d3796dc90e962dfcd851920377
yellowmodric <kyr7091@naver.com>
2025-04-09 11:04:19 +0900

    거래내역 페이지 완료

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import 'bootstrap/dist/css/bootstrap.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'flatpickr/dist/flatpickr.css';
 
 import App from './App.vue';
 import router from './router';
